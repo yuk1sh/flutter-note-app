72393706aa671d3aabdd3790b7a988084ec20c6a
yuk1sh <139573151+yuk1sh@users.noreply.github.com>
2024-02-18 15:52:39 +0900

    v0.0.2

--- backend/app/controllers/journals_controller.rb
+++ backend/app/controllers/journals_controller.rb
@@ -1,2 +1,34 @@
 class JournalsController < ApplicationController
+	def index
+		journals = Journal.all
+		render json: journals
+	end
+
+	def show
+		journal = Journal.find(params[:id])
+		render json: journal
+	end
+
+	def create
+		journal = Journal.create(journal_params)
+		render json: journal
+	end
+
+	def update
+		journal = Journal.find(params[:id])
+		journal.update(journal_params)
+		render json: journal
+	end
+
+	def destroy
+		journal = Journal.find(params[:id])
+		journal.destroy
+		render json: journal
+	end
+
+	private
+
+	def journal_params
+		params.require(:journal).permit(:title, :content)
+	end
 end
